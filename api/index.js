0cbb09de4cb00f62fed1fe081fe2668841aa0fc6
Adel Ibrahim <adelanwer12@yahoo.com>
2024-04-20 22:38:11 +0200

    Create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter  from './routes/user.route.js';
 
 dotenv.config();
 const db = mongoose.connect(process.env.MONGO).then(() => {
@@ -11,9 +12,7 @@ const db = mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('I Learn Express');
-});
+app.use('/api/user', userRouter);
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
