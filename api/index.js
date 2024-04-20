047ae8e628990c7d81d3b53a17a4bac9d5f367bc
Adel Ibrahim <adelanwer12@yahoo.com>
2024-04-20 23:39:49 +0200

    Create Sign up Api Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter  from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 const db = mongoose.connect(process.env.MONGO).then(() => {
@@ -11,8 +12,10 @@ const db = mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
